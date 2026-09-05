e023f8b96b9473afb10e1e47691b59d77a719f55
Sharmista2410013 <saha.sharmi06@gmail.com>
2026-09-06 01:20:28 +0600

    Update Helper.js

--- Helper.js
+++ Helper.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
 
 const helperSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true, lowercase: true },
+    id: { type: String },
+    name: { type: String, required: true, lowercase: true },
+    identifier: { type: String }, // Stores email or phone for login
+    password: { type: String },   // Stores login password
+    inst: { type: String },       // Institution Name
+    dept: { type: String },       // Department
     phone: { type: String, required: true },
     area: { type: String, required: true },
     skills: { type: Array, default: [] },
-    bio: { type: String, default: '' }
+    bio: { type: String, default: '' },
+    registeredAt: { type: String }
 });
 
 module.exports = mongoose.model('Helper', helperSchema);

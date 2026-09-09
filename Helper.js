de6f9251b1e9e41a46f64f616a947b9c561cb2d1
Sharmista2410013 <saha.sharmi06@gmail.com>
2026-09-09 22:54:09 +0600

    Update Helper.js for Forgotten Password

--- Helper.js
+++ Helper.js
@@ -1,17 +1,20 @@
 const mongoose = require('mongoose');
 
 const helperSchema = new mongoose.Schema({
-    id: { type: String },
-    name: { type: String, required: true, lowercase: true },
-    identifier: { type: String }, // Stores email or phone for login
-    password: { type: String },   // Stores login password
-    inst: { type: String },       // Institution Name
-    dept: { type: String },       // Department
-    phone: { type: String, required: true },
-    area: { type: String, required: true },
-    skills: { type: Array, default: [] },
+    id: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    identifier: { type: String, required: true, unique: true, lowercase: true },
+    password: { type: String, required: true },
+    phone: { type: String, default: '' },
+    inst: { type: String, default: '' },
+    dept: { type: String, default: '' },
+    area: { type: String, default: '' },
+    skills: { type: [String], default: [] },
     bio: { type: String, default: '' },
-    registeredAt: { type: String }
+    registeredAt: { type: String, required: true },
+    // Fields for Forgot & Reset Password
+    resetPasswordCode: { type: String },
+    resetPasswordExpires: { type: Date }
 });
 
 module.exports = mongoose.model('Helper', helperSchema);

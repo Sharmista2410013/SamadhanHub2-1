d7571ab8295a42034177cf4ac78d722f65c3c648
Sharmista2410013 <saha.sharmi06@gmail.com>
2026-09-10 06:19:13 +0600

    Update Customer.js for Final Resend

--- Customer.js
+++ Customer.js
@@ -4,7 +4,7 @@ const customerSchema = new mongoose.Schema({
     id: { type: String, required: true, unique: true },
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true, lowercase: true },
-    password: { type: String, required: true }, // In a production app, we would hash this, but keeping it simple for your project!
+    password: { type: String, required: true },
     phone: { type: String, default: '' },
     area: { type: String, default: '' },
     registeredAt: { type: String, required: true },
